b8c7d13d177c75e5576924a1a22bddcdc3c8168b
MardenRocha <43004656+MardenRocha@users.noreply.github.com>
2019-10-02 17:15:06 -0300

    fix the Search component form.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
   }
 
   onChange = e => {
+    e.preventDefault();
     this.setState({ search: e.target.value });
   };
 
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -5,7 +5,11 @@ import { Link } from "react-router-dom";
 const Search = ({ onChange, value }) => (
   <div className="row justify-content-center mt-5">
     <div className="col-8">
-      <form className="form-inline justify-content-center">
+      <form
+        className="form-inline justify-content-center"
+        action={`/user/${value}`}
+        method="GET"
+      >
         <input
           type="text"
           className="form-control w-50 ml-2"
